a028a86eb8cf050606ecc3ed3ace751d2f6403b5
Eliza Fernando <keaf2004@gmail.com>
2023-12-09 13:10:51 +0530

    booking

--- booking.js
+++ booking.js
@@ -14,35 +14,14 @@ document.addEventListener('alpine:init', () =>{
             number_of_adults: '',
             number_of_children:'',
             extra_bed: '',
+        },
+
+        cost: {
+            singleRoom: '',
+            NumberOfSingleRooms: '',
+            NumberOfDoubleRooms: '',
+            NumberOfTripleRooms: '',
         }
     });
-
-    get_total_adventure_cost(); {
-        let total = 0;
-        total += this.booking.adventure.number_of_adults_local * 1000;
-        total += this.booking.adventure.number_of_adults_foreign * 2000;
-        total += this.booking.adventure.number_of_children_local * 500;
-        total += this.booking.adventure.number_of_children_foreign * 1000;
-        total += this.booking.adventure.adult_guide * 1000;
-        total += this.booking.adventure.child_guide * 500;
-        this.booking.final_adventure_cost = total;
-        return total;
-    };
-
-    get_total_cost(); {
-        let total = 0;
-        total += this.booking.room_cost * this.booking.number_of_rooms;
-        total += this.total_adventure_cost;
-        this.booking.total_cost = total;
-
-        // calculate discount
-        this.booking.total_discount = total * (this.booking.discount_percentage / 100);
-
-        // calculate final total
-        this.booking.final_total = total - this.booking.total_discount;
-
-        return total;
-
-    }
 })
     
